239f4fcc0f4e85cfad7412dc43ee89185f29096a
kodakyellow <zi5253hao@gmail.com>
2018-04-07 09:43:42 -0700

    Deleting testing logs

--- src/react/components/App.js
+++ src/react/components/App.js
@@ -4,11 +4,9 @@ import Home from './Home'
 import Creation from './Creation'
 
 const App = () => {
-  // console.log("INside App in app.js");
   return ( 
     <Router>
       <div>
-        
         <Route exact path="/" component={Home} />
         <Route exact path="/creation" component={Creation} />
       </div>
